039a5ab64f5cbdb13b1089ad92911ff6cd4c365c
Affyalqorna10 <Affyalqorna10@gmail.com>
2023-06-25 20:10:29 +0700

    Menambahkan Update KaryawanController

--- src/main/java/com/data/sewalaptop/controller/master/KaryawanController.java
+++ src/main/java/com/data/sewalaptop/controller/master/KaryawanController.java
@@ -1,12 +1,17 @@
 package com.data.sewalaptop.controller.master;
 
+import com.data.sewalaptop.dto.master.MstDevicesDTO;
 import com.data.sewalaptop.dto.master.MstKaryawanDTO;
+import com.data.sewalaptop.model.master.MstDevices;
+import com.data.sewalaptop.model.master.MstKaryawan;
+import com.data.sewalaptop.repository.master.KaryawanRepository;
 import com.data.sewalaptop.service.master.JwtService;
 import com.data.sewalaptop.service.master.KaryawanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -17,19 +22,15 @@ public class KaryawanController {
     private KaryawanService karyawanService;
 
     @Autowired
+    private KaryawanRepository karyawanRepository;
+
+    @Autowired
     JwtService jwtService;
 
     @PostMapping("/save")
     public ResponseEntity<?> saveKaryawan(@RequestBody MstKaryawanDTO request){
 
         return karyawanService.savekaryawan(request);
-    }
-
-    @DeleteMapping("/delete/{karyawanId}")
-    public ResponseEntity<?> saveKaryawan(@RequestHeader Map<String,String> header, @PathVariable Long karyawanId){
-
-        jwtService.filter(header);
-        return karyawanService.deleteKaryawan(karyawanId);
     }
 
     @GetMapping("/get_karyawan_by/{karyawanId}")
@@ -50,5 +51,36 @@ public class KaryawanController {
 
         jwtService.filter(header);
         return karyawanService.getAll();
+    }
+
+    @PutMapping("/update")
+    public Map<String, Object> updateDevice (@RequestHeader Map<String,String> header,
+                                             @RequestParam(value = "karyawanId")
+                                             Long karyawanId,
+                                             @RequestBody MstKaryawanDTO mstKaryawanDto){
+        Map<String, Object> mapResult = new HashMap<>();
+
+        MstKaryawan mstKaryawan = karyawanRepository.findById(karyawanId).orElse(null);
+
+        mstKaryawan.setDivisiId(mstKaryawanDto.getDivisiId());
+        mstKaryawan.setNikKaryawan(mstKaryawanDto.getNikKaryawan());
+        mstKaryawan.setNamaDepan(mstKaryawanDto.getNamaDepan());
+        mstKaryawan.setNamaBelakang(mstKaryawanDto.getNamaBelakang());
+        mstKaryawan.setEmailKaryawan(mstKaryawanDto.getEmailKaryawan());
+        mstKaryawan.setAlamatKaryawan(mstKaryawanDto.getAlamatKaryawan());
+        mstKaryawan.setTelpKaryawan(mstKaryawanDto.getTelpKaryawan());
+
+        mapResult.put("message", "Update success");
+        mapResult.put("data", karyawanRepository.save(mstKaryawan));
+
+        jwtService.filter(header);
+        return mapResult;
+    }
+
+    @DeleteMapping("/delete/{karyawanId}")
+    public ResponseEntity<?> saveKaryawan(@RequestHeader Map<String,String> header, @PathVariable Long karyawanId){
+
+        jwtService.filter(header);
+        return karyawanService.deleteKaryawan(karyawanId);
     }
 }
